12b2bf7255e4769832fae9e277af08dcc0289a32
Yeeun Jung <rjyeeun@gmail.com>
2022-12-13 22:49:30 -0500

    commit tree c604ec73b26e5a3d061bba9e204f969848e03d87

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+const element = document.getElementById("main");
+element.remove()
+const newHeader = document.createElement("h1")
+newHeader.setAttribute("id","victory")
+newHeader.textContent = "Yeeun is the champion";
